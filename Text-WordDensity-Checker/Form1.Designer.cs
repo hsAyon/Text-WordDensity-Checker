03e516cdba6b888dde491eb0aaeda9e07bb4e9e2
Hassan Shahriar Ayon <triplej7000@gmail.com>
2021-05-01 22:14:41 +0600

    Add project files.

---
+++ Text-WordDensity-Checker/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Text_WordDensity_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tbSearchString = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnClearPrev = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel5, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel6, 2, 0);
+            this.tableLayoutPanel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1011, 646);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbSource);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 103);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(398, 540);
+            this.panel1.TabIndex = 0;
+            // 
+            // tbSource
+            // 
+            this.tbSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSource.Location = new System.Drawing.Point(0, 0);
+            this.tbSource.MaxLength = 0;
+            this.tbSource.Multiline = true;
+            this.tbSource.Name = "tbSource";
+            this.tbSource.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbSource.Size = new System.Drawing.Size(398, 540);
+            this.tbSource.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbSearchString);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(407, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(196, 540);
+            this.panel2.TabIndex = 1;
+            // 
+            // tbSearchString
+            // 
+            this.tbSearchString.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbSearchString.Location = new System.Drawing.Point(0, 0);
+            this.tbSearchString.MaxLength = 0;
+            this.tbSearchString.Multiline = true;
+            this.tbSearchString.Name = "tbSearchString";
+            this.tbSearchString.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbSearchString.Size = new System.Drawing.Size(196, 540);
+            this.tbSearchString.TabIndex = 0;
+            this.tbSearchString.WordWrap = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tbOutput);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(609, 103);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(399, 540);
+            this.panel3.TabIndex = 2;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbOutput.Location = new System.Drawing.Point(0, 0);
+            this.tbOutput.MaxLength = 0;
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOutput.Size = new System.Drawing.Size(399, 540);
+            this.tbOutput.TabIndex = 0;
+            this.tbOutput.WordWrap = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnSource);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(398, 94);
+            this.panel4.TabIndex = 3;
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(3, 3);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(109, 88);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Open Source";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnSearch);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(407, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(196, 94);
+            this.panel5.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(3, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(114, 91);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.btnClearAll);
+            this.panel6.Controls.Add(this.btnClearPrev);
+            this.panel6.Controls.Add(this.btnSave);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(609, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(399, 94);
+            this.panel6.TabIndex = 5;
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(288, 51);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(108, 40);
+            this.btnClearAll.TabIndex = 2;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnClearPrev
+            // 
+            this.btnClearPrev.Location = new System.Drawing.Point(288, 3);
+            this.btnClearPrev.Name = "btnClearPrev";
+            this.btnClearPrev.Size = new System.Drawing.Size(108, 42);
+            this.btnClearPrev.TabIndex = 1;
+            this.btnClearPrev.Text = "Clear Last";
+            this.btnClearPrev.UseVisualStyleBackColor = true;
+            this.btnClearPrev.Click += new System.EventHandler(this.btnClearPrev_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(3, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(113, 88);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save CSV";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 646);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Keyword Density Checker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formClose_Click);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.TextBox tbSearchString;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClearPrev;
+        private System.Windows.Forms.Button btnClearAll;
+    }
+}
+
---
+++ Text-WordDensity-Checker/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_WordDensity_Checker
+{
+    public partial class Form1 : Form
+    {
+        List<List<string>> listOutput = new List<List<string>>();
+        int countPages = 1;
+
+        public void showOutput()
+        {
+            tbOutput.Clear();
+            for (int i = 0; i < listOutput[0].Count; i++)
+            {
+                for (int j = 0; j < listOutput.Count; j++)
+                {
+                    tbOutput.Text += listOutput[j][i] + "\t";
+                }
+                tbOutput.Text += "\r\n";
+            }
+        }
+
+        public Form1()
+        {
+            listOutput.Add(new List<string>());
+            listOutput[0].Add("");
+            listOutput[0].Add("Word");
+
+            InitializeComponent();
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openSource = new OpenFileDialog();
+            openSource.Title = "Open source text";
+            
+            if (openSource.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openSource.FileName;
+                tbSource.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            tbSearchString.Text = tbSearchString.Text.Replace('\u00a0', '\u0020');
+            tbSource.Text = tbSource.Text.Replace('\u00a0', '\u0020');
+
+            string[] searchWords = tbSearchString.Text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var countAllWords = tbSource.Text.Split(new[] { " ", "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).Length;
+            
+            int nTerm = 1 + (countPages - 1) * 2; // nth term of arithmatic sequence (a1 = 1, d = 2)
+
+            listOutput.Add(new List<string>());
+            listOutput.Add(new List<string>());
+
+            listOutput[nTerm].Add("Page "+countPages);
+            listOutput[nTerm].Add("Count");
+            listOutput[nTerm + 1].Add("");
+            listOutput[nTerm + 1].Add("Density");
+
+            foreach (var word in searchWords)
+            {
+                if (countPages <= 1)
+                {
+                    listOutput[0].Add(word);
+                }
+
+                string regex = @"\b"+word+@"\b";
+                int countMatches = Regex.Matches(tbSource.Text, regex, RegexOptions.IgnoreCase).Count;
+
+                listOutput[nTerm].Add(countMatches.ToString());
+                listOutput[nTerm + 1].Add(((float)countMatches / (float)countAllWords).ToString());
+            }
+
+            countPages++;
+
+            if(countPages != 1)
+            {
+                tbSearchString.ReadOnly = true;
+            }
+
+            showOutput();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveCSV = new SaveFileDialog();
+            saveCSV.Title = "Save CSV";
+            saveCSV.DefaultExt = "csv";
+            
+            if (saveCSV.ShowDialog() == DialogResult.OK)
+            {
+                string finalCSV = "";
+
+                for (int i = 0; i < listOutput[0].Count; i++)
+                {
+                    for (int j = 0; j < listOutput.Count; j++)
+                    {
+                        finalCSV += listOutput[j][i] + ",";
+                    }
+                    finalCSV += "\r\n";
+                }
+
+                string filePath = saveCSV.FileName;
+                File.WriteAllText(filePath, finalCSV);
+            }
+        }
+
+        private void btnClearPrev_Click(object sender, EventArgs e)
+        {
+            DialogResult confClear = MessageBox.Show("Are you sure you want to clear the last output?", "Clear Confirmation", MessageBoxButtons.YesNo);
+            if (confClear == DialogResult.Yes)
+            {
+                if (countPages > 1)
+                {
+                    listOutput.Remove(listOutput.Last());
+                    listOutput.Remove(listOutput.Last());
+                    countPages--;
+                    showOutput();
+                }
+            }
+            else if (confClear == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            DialogResult confClear = MessageBox.Show("Are you sure you want to clear all output?", "Clear Confirmation", MessageBoxButtons.YesNo);
+            if (confClear == DialogResult.Yes)
+            {
+                tbOutput.Clear();
+                countPages = 1;
+
+                listOutput.Clear();
+
+                listOutput.Add(new List<string>());
+                listOutput[0].Add("");
+                listOutput[0].Add("Word");
+
+                tbSearchString.ReadOnly = false;
+            }
+            else if (confClear == DialogResult.No)
+            {
+                
+            }          
+        }
+
+        private void formClose_Click(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you really want to close the program?", "Exit Confirmation", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+            else if (dialog == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
